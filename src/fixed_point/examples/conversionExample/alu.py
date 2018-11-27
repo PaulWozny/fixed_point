cbfea1630d711974fa555864faf5b346d4ef43f6
Paul Wozny <pawel0wozny@gmail.com>
2018-11-27 18:23:29 +0100

    fighting with conversion

--- src/fixed_point/examples/conversionExample/alu.py
+++ src/fixed_point/examples/conversionExample/alu.py
@@ -3,22 +3,49 @@ from fixed_point import *
 
 
 @block
-def adder(A,B, output2, clk, reset):
+def adderClk(A, B, output2):
     """
 
     :param A:  input
     :param B: input
     :param output2: output
-    :param clk:
-    :param reset:
     :return:
     """
 
-    @always_seq(clk.posedge, reset=reset)
+    clk = Signal(0)
+    @always(delay(10))
+    def drive_clk():
+        clk.next = not clk
+
+    @always(clk.posedge)
+    # @always_comb
     def seq():
-        output2.next = A+B
+        print(A)
+        print(B)
+        sum = A + B
+        print(sum)
+        # retVal = fixbv(sum._fval, format=sum._W)
+        retVal = sum
+        output2.next = retVal
+        print(output2)
 
-    return seq
+    return drive_clk, seq
 
+# @block
+# def adder(A, B, output2):
+#     """
+#
+#     :param A:  input
+#     :param B: input
+#     :param output2: output
+#     :return:
+#     """
+#
+#
+#     @always_comb
+#     def seq():
+#         output2.next = A + B
+#
+#     return seq
 
 
--- src/fixed_point/examples/conversionExample/alu_tb.py
+++ src/fixed_point/examples/conversionExample/alu_tb.py
@@ -1,38 +1,39 @@
-from  myhdl import Signal, intbv, conversion, ResetSignal
-from alu import adder
+from alu import adderClk
 from fixed_point import fixbv
+from myhdl import *
+
 
 def adder_tb():
 
-    # x1 = fixbv(0.22, min=-1, max=1, res=2**-15) # this is bad
-    # x2 = fixbv(0.22, min=-1, max=1, res=2**-15)
-    # output = fixbv(0.22, min=-1, max=1, res=2**-15)
+    x1 = Signal(fixbv(1.5, min=-2, max=2, res=2**-15))
+    x2 = Signal(fixbv(0.25, min=-2, max=2, res=2**-15))
+    output = Signal(fixbv(0, min=-2, max=4, res=2**-15))
+    add = adderClk(x1, x2, output)
+    return add
 
-    x1 = Signal(fixbv(0.22, min=-1, max=1, res=2**-15))
-    x2 = Signal(fixbv(0.22, min=-1, max=1, res=2**-15))
-    output = Signal(fixbv(0, min=-1, max=1, res=2**-15))
+inst = adder_tb()
+inst.run_sim(50)
+# twoja = adder_tb()
+# twoja.convert(hdl='VHDL')
 
 
 
-    print(hex(x1))
-    print(hex(x1 + x2))
-    # x1 = intbv(32, min=-1000, max=10000)
-    # x2 = intbv(22, min=-1000, max=10000)
-    # output = intbv(0, min=-1000, max=10000)
+# x1 = Signal(fixbv(1.5, min=-2, max=2, res=2**-15))
+# x2 = Signal(fixbv(0.25, min=-2, max=2, res=2**-10))
+# out = Signal(fixbv(0, min=-4, max=4, res=2**-20))
+# out=x1+x2
+# print(out)
+# print(out._val)
+# print(out.res)
 
-    # x1 = Signal(intbv(32, min=-1000, max=10000))
-    # x2 = Signal(intbv(22, min=-1000, max=10000))
-    # output = Signal(intbv(0, min=-1000, max=10000))
+# x1 = Signal(fixbv(0, min=0, max=2, res=2**-15))
+# x2 = Signal(fixbv(0.25, min=0, max=2, res=2**-10))
+# output = Signal(fixbv(0, min=-4, max=4, res=2**-20))
 
-    clk = Signal(bool(0))
-    reset= ResetSignal(0, active=0, async=True)
-    add = adder(x1, x2, output, clk, reset)
-
-    add.convert(hdl='VHDL')
-    add.convert(hdl='Verilog')
-
-    output.read
-
-adder_tb()
-
-# print(output.read)
+#
+# add = adder(x1, x2, output)
+#
+# x1 = Signal(fixbv(1.5, min=-2, max=2, res=2**-15))
+#
+# print(output)
+#
